3b96800bd212a315fda68cf052117be538f73c19
Eden Moyal <edenmoyal95@gmail.com>
2026-05-31 21:20:30 +0300

    Deployment preperations

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import routineRoutes from "./routes/route";
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -9,5 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/routines", routineRoutes);
+
+const distPath = path.resolve(__dirname, "..", "client", "dist");
+app.use(express.static(distPath));
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
 
 export { app };
