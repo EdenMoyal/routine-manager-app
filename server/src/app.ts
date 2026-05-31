e4a30da23d8cfea7fa24988578f442136abe8996
Eden Moyal <edenmoyal95@gmail.com>
2026-05-31 22:39:20 +0300

    Fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,10 +11,10 @@ app.use(express.json());
 
 app.use("/api/routines", routineRoutes);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.get(/.*$/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
 export { app };
